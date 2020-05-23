1183c8f78458642df67b6eff2da3794f7c30b735
Hamilkar247 <adonnibal96@gmail.com>
2020-05-23 22:10:33 +0200

    WO-31 skrypt do testów

---
+++ testLink.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+match="$(cat searchxyz~)"
+#1
+if [[ "$1" == "1" ]]
+  then
+    google --rua "$match"
+#2
+elif [[ "$1" == "2" ]]
+  then
+    google --rua "$match" | sed 's/_/ /'
+#3
+elif [[ "$1" == "3" ]]
+  then
+    google --rua "$match" | sed 's/_/ /' | sed 's/bonusy/titulky/'
+#4
+elif [[ "$1" == "4" ]]
+  then
+    google --rua "$match" | sed 's/_/ /' | sed 's/bonusy/titulky/' | sed 's/dalsi-casti/titulky/'
+#5
+elif [[ "$1" == "5" ]]
+  then
+    awk -f command_search.awk result_search.txt~
+else
+  "nie podałeś numeru parametru"
+fi
